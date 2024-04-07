7f1ea94c56e9577ec89a7adbd82e54f44c6f17db
beamiter <beamiter@163.com>
2024-04-07 17:57:49 +0800

    Use vte-sys
    Signed-off-by: beamiter <beamiter@163.com>

--- jterm2/src/main.rs
+++ jterm2/src/main.rs
@@ -1,3 +1,9 @@
+extern crate vte;
+extern crate gtk;
+use glib::{*, ffi::gboolean};
+use gtk::ffi::GtkWidget;
+use vte_sys::VteTerminal;
+
 const PCRE2_CODE_UNIT_WIDTH: i8 = 8;
 
 enum ConfigItemType {
@@ -8,13 +14,36 @@ enum ConfigItemType {
     UINT64,
 }
 
+// With tagged variants.
+enum ConfigUnion {
+    s(Vec<char>),
+    sl(Vec<Vec<char>>),
+    b (gboolean),
+    i(i64),
+    ui(u64),
+}
+
 struct ConfigItem {
     s: Vec<char>,
     n: Vec<char>,
     t: ConfigItemType,
     l: u64,
+    v: ConfigUnion,
+}
+
+struct Terminal {
+    hold: gboolean,
+    term: *mut GtkWidget,
+    win: *mut GtkWidget,
+    has_child_exit_status: gboolean,
+    child_exit_status: i64,
+    current_font: usize,
+}
+
+fn cb_spawn_async() {
+    
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("evil");
 }
