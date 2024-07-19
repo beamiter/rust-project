2c0d9fa9c480af2ba190ce5238ab2c7ea941219e
beamiter <beamiter@163.com>
2024-07-19 17:24:28 +0800

    window and terminal work
    Signed-off-by: beamiter <beamiter@163.com>

--- jterm4/src/main.rs
+++ jterm4/src/main.rs
@@ -1,11 +1,13 @@
-use gtk4 as gtk;
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow};
+use gtk4 as gtk;
+use gtk4::gio::Cancellable;
+use gtk4::glib::SpawnFlags;
+use vte4::TerminalExtManual;
+use vte4::{PtyFlags, Terminal};
 
 fn main() -> glib::ExitCode {
-    let app = Application::builder()
-        .application_id("org.example.HelloWorld")
-        .build();
+    let app = Application::builder().application_id("app.jterm4").build();
 
     app.connect_activate(|app| {
         // We create the main window.
@@ -13,8 +15,39 @@ fn main() -> glib::ExitCode {
             .application(app)
             .default_width(320)
             .default_height(200)
-            .title("Hello, World!")
+            .title("jterm4")
+            .name("win_name")
+            .opacity(0.8)
             .build();
+
+        let terminal = Terminal::builder()
+            .hexpand(true)
+            .vexpand(true)
+            .name("term_name")
+            .allow_hyperlink(true)
+            .bold_is_bright(true)
+            .scrollback_lines(5000)
+            .opacity(0.5)
+            .pointer_autohide(true)
+            .build();
+        let argv = &["/bin/bash", "-/bin/bash"];
+        let envv: &[&str] = &[];
+        let spawn_flags = SpawnFlags::SEARCH_PATH | SpawnFlags::FILE_AND_ARGV_ZERO;
+        let cancellable: Option<&Cancellable> = None;
+        let working_directory = Some("~/");
+        terminal.spawn_async(
+            PtyFlags::DEFAULT,
+            working_directory,
+            argv,
+            envv,
+            spawn_flags,
+            || println!("haha"),
+            -1,
+            cancellable,
+            |res| println!("{:?}", res),
+        );
+
+        window.set_child(Some(&terminal));
 
         // Show the window.
         window.present();
