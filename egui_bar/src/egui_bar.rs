53bba1c90aa81422278bdae911a4ac8ceaa5f63f
beamiter <beamiter@163.com>
2025-01-21 22:28:00 +0800

    fine egui bar

--- egui_bar/src/egui_bar.rs
+++ egui_bar/src/egui_bar.rs
@@ -3,31 +3,37 @@ use std::fs::File;
 use std::io::Read;
 
 pub struct MyEguiApp {
-    pipe: File,
     message: String,
+    pipe: std::io::Result<File>,
 }
 
 impl MyEguiApp {
     pub fn new(pipe_path: String) -> Self {
         Self {
-            pipe: File::open(pipe_path).unwrap(),
             message: String::new(),
+            pipe: File::open(pipe_path),
         }
     }
 }
 
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // 从管道读取消息
-        let mut buffer = [0; 1024];
-        if let Ok(size) = self.pipe.read(&mut buffer) {
-            if size > 0 {
-                self.message = String::from_utf8_lossy(&buffer[..size]).to_string();
+        match self.pipe {
+            Ok(ref mut pipe) => {
+                let mut buffer = [0; 1024];
+                if let Ok(size) = pipe.read(&mut buffer) {
+                    if size > 0 {
+                        self.message = String::from_utf8_lossy(&buffer[..size]).to_string();
+                    } else {
+                        self.message = "empty pipe".to_string();
+                    }
+                }
+            }
+            Err(_) => {
+                self.message = "fail to open pipe".to_string();
             }
         }
-        // 显示消息
         egui::CentralPanel::default().show(ctx, |ui| {
-            println!("pipe info: {}", self.message);
             ui.label(&self.message);
         });
     }
--- egui_bar/src/main.rs
+++ egui_bar/src/main.rs
@@ -5,10 +5,15 @@ use std::env;
 fn main() -> eframe::Result {
     let args: Vec<String> = env::args().collect();
     let pipe_path = &args[1];
-    let options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1800.0, 50.0])
+            .with_min_inner_size([1000.0, 40.0]),
+        ..Default::default()
+    };
     eframe::run_native(
         "My egui App",
-        options,
+        native_options,
         Box::new(|_cc| Ok(Box::new(MyEguiApp::new(pipe_path.to_string())))),
     )
 }
