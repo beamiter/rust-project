e2ca087f5ddbc551f590c146a72fd060e34f7d45
beamiter <beamiter@163.com>
2025-01-22 12:06:55 +0800

    set max height

--- egui_bar/src/main.rs
+++ egui_bar/src/main.rs
@@ -7,10 +7,11 @@ fn main() -> eframe::Result {
     let pipe_path = &args[1];
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1000.0, 50.0]) // Initial height
-            .with_min_inner_size([1000.0, 40.0]) // Minimum size
+            .with_inner_size([1000.0, 20.0]) // Initial height
+            .with_min_inner_size([1000.0, 20.0]) // Minimum size
+            .with_max_inner_size([f32::INFINITY, 20.0]) // Set max height to 100.0
             .with_decorations(false), // Hide title bar and decorations
-            // .with_always_on_top(), // Keep window always on top
+        // .with_always_on_top(), // Keep window always on top
         ..Default::default()
     };
     eframe::run_native(
