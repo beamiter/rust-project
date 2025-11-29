659205a38c3ba584155cd0a9bc116c1902347140
beamiter <beamiter@163.com>
2025-11-29 09:34:35 +0800

    refresh

--- jwm/src/backend/wayland/handlers/mod.rs
+++ jwm/src/backend/wayland/handlers/mod.rs
@@ -1,22 +1,24 @@
 mod compositor;
 mod xdg_shell;
 
+use super::Smallvil;
+
 //
 // Wl Seat
 //
 
+use smithay::input::dnd::{DnDGrab, DndGrabHandler, GrabType, Source};
+use smithay::input::pointer::Focus;
 use smithay::input::{Seat, SeatHandler, SeatState};
 use smithay::reexports::wayland_server::Resource;
 use smithay::reexports::wayland_server::protocol::wl_surface::WlSurface;
+use smithay::utils::Serial;
 use smithay::wayland::output::OutputHandler;
 use smithay::wayland::selection::SelectionHandler;
 use smithay::wayland::selection::data_device::{
-    ClientDndGrabHandler, DataDeviceHandler, DataDeviceState, ServerDndGrabHandler,
-    set_data_device_focus,
+    DataDeviceHandler, DataDeviceState, WaylandDndGrabHandler, set_data_device_focus,
 };
 use smithay::{delegate_data_device, delegate_output, delegate_seat};
-
-use super::Smallvil;
 
 impl SeatHandler for Smallvil {
     type KeyboardFocus = WlSurface;
@@ -57,8 +59,32 @@ impl DataDeviceHandler for Smallvil {
     }
 }
 
-impl ClientDndGrabHandler for Smallvil {}
-impl ServerDndGrabHandler for Smallvil {}
+impl DndGrabHandler for Smallvil {}
+impl WaylandDndGrabHandler for Smallvil {
+    fn dnd_requested<S: Source>(
+        &mut self,
+        source: S,
+        _icon: Option<WlSurface>,
+        seat: Seat<Self>,
+        serial: Serial,
+        type_: GrabType,
+    ) {
+        match type_ {
+            GrabType::Pointer => {
+                let ptr = seat.get_pointer().unwrap();
+                let start_data = ptr.grab_start_data().unwrap();
+
+                // create a dnd grab to start the operation
+                let grab = DnDGrab::new_pointer(&self.display_handle, start_data, source, seat);
+                ptr.set_grab(self, grab, serial, Focus::Keep);
+            }
+            GrabType::Touch => {
+                // smallvil lacks touch handling
+                source.cancel();
+            }
+        }
+    }
+}
 
 delegate_data_device!(Smallvil);
 
