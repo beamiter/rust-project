d5986518c405adf9ca826c6db205b7b9288feb05
beamiter <beamiter@163.com>
2026-02-11 14:32:36 +0800

    update

--- jwm/ha.sh
+++
@@ -1,1 +0,0 @@
-env RUST_LOG='jwm=info,jwm::backend::udev=debug,smithay::backend::libinput=debug,smithay::input::keyboard=debug' JWM_BACKEND=wayland-udev JWM_DEBUG_KEYS=1 JWM_AUTOSTART_TERMINAL=1 jwm
